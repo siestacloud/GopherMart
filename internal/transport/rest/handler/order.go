1f9060fb7671349cb3bf0c2f4adea26d75452d6d
Siesta <siesta.cloud@gmail.com>
2022-09-11 15:42:46 +0300

    fix get all client orders endpoint

--- internal/transport/rest/handler/order.go
+++ internal/transport/rest/handler/order.go
@@ -97,7 +97,7 @@ func (h *Handler) GetOrders() echo.HandlerFunc {
 			return errResponse(c, http.StatusInternalServerError, "internal server error")
 		}
 
-		// respList := []core.Order{}
+		respList := []core.Order{}
 		for i, _ := range orderList {
 			// * получаю информацию о расчете начислений баллов лояльности (внешнее api)
 			if err := h.services.Accrual.GetOrderAccrual(&orderList[i]); err != nil {
@@ -106,7 +106,6 @@ func (h *Handler) GetOrders() echo.HandlerFunc {
 			}
 			if orderList[i].Status == "" {
 				orderList[i].Status = "PROCESSING"
-				// respList = append(respList, orderList[i])
 			}
 		}
 
@@ -116,8 +115,9 @@ func (h *Handler) GetOrders() echo.HandlerFunc {
 		// }
 		c.Request().Header.Set("Content-Type", "application/json")
 
-		pkg.InfoPrint("transport", "OK", orderList)
-		return c.JSON(http.StatusOK, orderList[len(orderList)-1])
+		respList = append(respList, orderList[len(orderList)-1])
+		pkg.InfoPrint("transport", "OK", respList)
+		return c.JSON(http.StatusOK, respList)
 
 	}
 }
