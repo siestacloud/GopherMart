568cf8dc46e59a7304976493fcc7db61667f80c5
Siesta <siesta.cloud@gmail.com>
2022-09-11 13:35:24 +0300

    fix get all client orders endpoint

--- internal/core/order.go
+++ internal/core/order.go
@@ -2,8 +2,8 @@ package core
 
 // Order имплементирует заказ клиента
 type Order struct {
-	Number     string `json:"number,omitempty" db:"user_order" validate:"required,numeric,max=20"`
-	Status     string `json:"status" db:"status"`
-	Accrual    int32  `json:"accrual"`
-	CreateTime string `json:"uploaded_at" db:"create_time"`
+	Number     string  `json:"number,omitempty" db:"user_order" validate:"required,numeric,max=20"`
+	Status     string  `json:"status,omitempty" db:"status"`
+	Accrual    float64 `json:"accrual,omitempty"`
+	CreateTime string  `json:"uploaded_at,omitempty" db:"create_time"`
 }
