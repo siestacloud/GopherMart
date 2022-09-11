2b2744debfcec3c77b348cf5dfbe4eecc2ce1dca
Siesta <siesta.cloud@gmail.com>
2022-09-11 14:21:56 +0300

    fix get all client orders endpoint

--- internal/transport/rest/handler/order.go
+++ internal/transport/rest/handler/order.go
@@ -118,7 +118,7 @@ func (h *Handler) GetOrders() echo.HandlerFunc {
 		c.Request().Header.Set("Content-Type", "application/json")
 
 		pkg.InfoPrint("transport", "OK", respList)
-		return c.JSON(http.StatusOK, orderList)
+		return c.JSON(http.StatusOK, respList)
 
 	}
 }
