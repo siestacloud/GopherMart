247e3d7e3c62a51b6b5778220cccf4b2d31cb5ee
Siesta <siesta.cloud@gmail.com>
2022-09-12 17:13:08 +0300

    set last endpoint

--- internal/core/order.go
+++ internal/core/order.go
@@ -7,7 +7,7 @@ type Order struct {
 	Number        string         `json:"number,omitempty" db:"user_order" validate:"required,numeric,max=20"`
 	Status        string         `json:"status,omitempty" db:"status"`
 	Accrual       float64        `json:"accrual,omitempty"`
-	Sum           float64        `json:"sum,omitempty" db:"sum" validate:"required"`
+	Sum           float64        `json:"sum,omitempty" db:"sum"`
 	CreateTime    sql.NullString `json:"uploaded_at,omitempty" db:"create_time"`
 	WithdrawnTime sql.NullString `json:"processed_at,omitempty" db:"withdrawn_time"`
 }
